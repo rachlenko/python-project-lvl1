a724ce5bc817228ca0ee7cffced363e46c29fbc2
Evgeny Rachlenko <evgeny.rachlenko@gmail.com>
2021-10-20 16:15:48 +0000

    bug fix

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -5,8 +5,8 @@ import prompt
 
 def welcome_user():
     print("Welcome to the Brain Games!")
-    username = prompt.string("May I have your name")
-    print(f"Hello, {username}")
+    username = prompt.string("May I have your name? ")
+    print(f"Hello, {username}!")
     return username
 
 
